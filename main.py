66f53aa985d4305019fc804387652c487b7ca4f5
Oranger <82070548+orangerthings@users.noreply.github.com>
2026-04-20 22:24:54 -0400

    update clean_html

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 
 def clean_html(html):
     # remove useless html filler slop
     soup = BeautifulSoup(html, "html.parser")
     for tag in soup(["script", "style", "noscript", "iframe", "svg"]):
         tag.decompose()
-    text = soup.get_text(separator="\n")
-    # remove empty lines
-    text = "\n".join([line for line in text.splitlines() if line.strip()])
+    for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
+        comment.extract()
+    # unwrap everything else
+    keep = {"a", "button", "h1", "h2", "h3", "h4", "h5", "h6", "p", "li", "ul", "ol"}
+    for tag in soup.find_all(True):
+        if tag.name not in keep:
+            tag.unwrap()
+    # remove empty lines and flatten into text
+    text = "\n".join([line for line in str(soup).splitlines() if line])
     return text
 
-url = "https://frc5190.com"
+url = "https://example.com"
 
 response = clean_html(requests.get(url).text)
 
