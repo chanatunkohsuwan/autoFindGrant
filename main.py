afcf54e98884316df573227fe2019ae1df325140
Oranger <82070548+orangerthings@users.noreply.github.com>
2026-04-20 20:53:53 -0400

    add pip modules

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
+
+def clean_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    for tag in soup(["script", "style", "noscript", "iframe", "svg"]):
+        tag.decompose()
+    return soup.get_text(separator="\n")
 
 url = "https://frc5190.com"
 
-response = requests.get(url)
+response = clean_html(requests.get(url).text)
 
-with open("output.out", "w+") as f:
-    f.write(response.text)
+with open("output.out", "w", encoding="utf-8") as f:
+    f.write(response)
