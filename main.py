023037b42ccab4d510e8d5a80e554d201a90fa56
Oranger <82070548+orangerthings@users.noreply.github.com>
2026-04-20 20:58:09 -0400

    update clean html

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def clean_html(html):
+    # remove useless html filler slop
     soup = BeautifulSoup(html, "html.parser")
     for tag in soup(["script", "style", "noscript", "iframe", "svg"]):
         tag.decompose()
-    return soup.get_text(separator="\n")
+    text = soup.get_text(separator="\n")
+    # remove empty lines
+    text = "\n".join([line for line in text.splitlines() if line.strip()])
+    return text
 
 url = "https://frc5190.com"
 
