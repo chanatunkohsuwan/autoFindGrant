faf9449923780d64bb778364f21e4bbd5b37d8de
chanatun <chanatun.kohsuwan@gmail.com>
2026-04-21 11:15:28 -0400

    added some more schemas for tables

--- upload.py
+++ upload.py
@@ -1,6 +1,5 @@
 from sqlalchemy import ForeignKey, String, DeclarativeBase, Mapped, mapped_column, relationship
 
-
 class team(Base):
     __tablename__ = "team"
     # team data
@@ -8,20 +7,27 @@ class team(Base):
     name: Mapped[str] = mapped_column()
     website_url: Mapped[str] = mapped_column()
     industry: Mapped[str] = mapped_column()
-    lat: Mapped[float] = mapped_column()
-    lng: Mapped[float] = mapped_column()
+    latitude: Mapped[float] = mapped_column()
+    longitude: Mapped[float] = mapped_column()
     worlds: Mapped[str] = mapped_column()
     state: Mapped[str] = mapped_column()
     tier: Mapped[str] = mapped_column()
-    source: Mapped[str] = mapped_column()
-    teams_sponsored: Mapped[list[int]] = mapped_column()
 
 class sponsor(Base):
+    # sponsor
     __tablename__ = "sponsor"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
     website_url: Mapped[str] = mapped_column()
     industry: Mapped[str] = mapped_column()
-    lat: Mapped[float] = mapped_column()
-    lng: Mapped[float] = mapped_column()
+    latitude: Mapped[float] = mapped_column()
+    longitude: Mapped[float] = mapped_column()
+
+class events(Base):
+    __tablename__ = "events"
+    # event data given team
+    id: Mapped[int] = mapped_column(primary_key=True)
+    event_code: Mapped[str] = mapped_column()
+    year: Mapped[int] = mapped_column()
+    team_id: Mapped[int] = mapped_column(ForeignKey("team.id"))
     
