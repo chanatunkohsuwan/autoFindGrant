c2979e1c99b148f88b5a11ee8c647168e48b9f29
Oranger <82070548+orangerthings@users.noreply.github.com>
2026-04-22 19:06:46 -0400

    update functions

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import re
 import requests
 from bs4 import BeautifulSoup, Comment
 
-def clean_html(html):
+def clean_soup(soup: BeautifulSoup):
     # remove useless html filler slop
-    soup = BeautifulSoup(html, "html.parser")
     for tag in soup(["script", "style", "noscript", "iframe", "svg"]):
         tag.decompose()
     for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
         comment.extract()
     # unwrap everything else
-    keep = {"a", "button", "h1", "h2", "h3", "h4", "h5", "h6", "p", "li", "ul", "ol"}
+    keep = {"a", "button", "img", "h1", "h2", "h3", "h4", "h5", "h6", "p", "li", "ul", "ol"}
     for tag in soup.find_all(True):
         if tag.name not in keep:
             tag.unwrap()
@@ -17,9 +17,15 @@ def clean_html(html):
     text = "\n".join([line for line in str(soup).splitlines() if line])
     return text
 
-url = "https://example.com"
+def find_sponsor_page(soup: BeautifulSoup):
+    # simple soup search for now but will add more complicated filtering if needed
+    return [a.get('href') for a in soup.find_all('a', href=True)]
 
-response = clean_html(requests.get(url).text)
+url = "https://frc5190.org/sponsors/"
+
+response = requests.get(url).text
+soup = BeautifulSoup(response, "html.parser")
+page = clean_soup(soup)
 
 with open("output.out", "w", encoding="utf-8") as f:
-    f.write(response)
+    f.write(page)
