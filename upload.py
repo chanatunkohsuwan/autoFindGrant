09b4425218cb3e3650e8b8df6cba65679f5d0e8e
Oranger <82070548+orangerthings@users.noreply.github.com>
2026-04-25 15:52:22 -0400

    fix meaningless attribubtes in upload.py

--- upload.py
+++ upload.py
@@ -5,15 +5,12 @@ import sqlalchemy # for sqlalchemy.null instead of just 'null'
 
 VALUES_TO_NULL = {"N/A", "null", "none", "", None}
 
-
 class Base(DeclarativeBase):
-
     def purge_null_data(self, data: dict):
         for key, value in data:
             if type(value) == str: value = value.lower()
             if VALUES_TO_NULL[value]: data[key] = sqlalchemy.null
         return data
-
 
 class team(Base):
     __tablename__ = "team"
@@ -22,13 +19,11 @@ class team(Base):
     name: Mapped[str] = mapped_column()
     website_url: Mapped[str] = mapped_column()
     city: Mapped[str] = mapped_column()
+    state: Mapped[str] = mapped_column()
     country: Mapped[str] = mapped_column()
-    industry: Mapped[str] = mapped_column()
     latitude: Mapped[float] = mapped_column()
     longitude: Mapped[float] = mapped_column()
     worlds: Mapped[str] = mapped_column()
-    state: Mapped[str] = mapped_column()
-    tier: Mapped[str] = mapped_column()
 
 class sponsor(Base):
     # sponsor
